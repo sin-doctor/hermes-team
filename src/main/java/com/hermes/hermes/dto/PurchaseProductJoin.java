5761599724a6cb53949cb236d77b54b8247ebdda
sin-doctor <sbs1602@naver.com>
2025-01-06 17:07:22 +0900

    hermes 111% complete

--- src/main/java/com/hermes/hermes/dto/PurchaseProductJoin.java
+++ src/main/java/com/hermes/hermes/dto/PurchaseProductJoin.java
@@ -1,8 +1,5 @@
 package com.hermes.hermes.dto;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
 
 import java.util.Date;
--- src/main/java/com/hermes/hermes/service/MyPageServiceImpl.java
+++ src/main/java/com/hermes/hermes/service/MyPageServiceImpl.java
@@ -1,6 +1,5 @@
 package com.hermes.hermes.service;
 
-import com.hermes.hermes.dto.Purchase;
 import com.hermes.hermes.dto.PurchaseProductJoin;
 import com.hermes.hermes.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +15,6 @@ public class MyPageServiceImpl implements MyPageService {
 
     @Autowired
     private UserMapper myPageMapper;
-
-    @Override
-    public List<Map<String, Object>> myPageService_getUserPurchase(String user_id) {
-        List<Purchase> purchaseList = myPageMapper.myPageService_getUserPurchase(user_id);
-        return purchaseList.stream().map(purchase -> {
-            Map<String, Object> purchaseMap = new HashMap<>();
-            purchaseMap.put("purchase_reg_num", purchase.getPurchase_reg_num());
-            purchaseMap.put("purchase_product_reg_num", purchase.getPurchase_product_reg_num());
-            purchaseMap.put("purchase_order_id", purchase.getPurchase_order_id());
-            purchaseMap.put("purchase_product_size", purchase.getPurchase_product_size());
-            purchaseMap.put("purchase_user_id", purchase.getPurchase_user_id());
-            purchaseMap.put("purchase_date", purchase.getPurchase_date());
-            purchaseMap.put("purchase_status", purchase.getPurchase_status());
-            return purchaseMap;
-        }).collect(Collectors.toList());
-    }
 
     @Override
     public List<Map<String, Object>> myPageService_getUserPurchase2(String user_id) {
